45febb8b989a854480c054d77c1d9aa2b6e0dfb5
brpuneet898 <brpuneet898@gmail.com>
2026-03-08 16:11:58 +0530

    added final comments on stopword removal

--- Cranfield_Dataset_NLP/code/stopwordRemoval.py
+++ Cranfield_Dataset_NLP/code/stopwordRemoval.py
@@ -7,8 +7,13 @@ from nltk.corpus import stopwords
 
 
 class StopwordRemoval():
-
+	## in this class we will be defining some functions that will help us in removing the stopwords from the text.
+	## what are stopwords? we can consider stopwords as those words which occur frequently in the corpus.
+	## since they occur in most of the documents, then they do not posses much discriminative power and 
+	## hence it is advised to remove them from the text .
 	def fromList(self, text):
+		## this function uses the standard ntlk stopword list of 198 words pre-defined in the english language.
+		## this function go through the corpus and wherever it encounters a stopword, that will get removed. 
 		stop_words = set(stopwords.words("english"))
 
 		stopwordRemovedText = []
